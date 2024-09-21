402b077419803a6a5d6264530e91decaaeaf0dc3
Vania2007 <momotis2007@gmail.com>
2024-09-21 14:16:59 +0300

    initial commit

---
+++ arithmetic-mean.php
@@ -0,0 +1,3 @@
+<?php
+$arr = [11, 12, 7, 10, 11, 11, 12];
+echo round(array_sum($arr) / count($arr), 2);
---
+++ reverse.php
@@ -0,0 +1,3 @@
+<?php
+$arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var_dump(array_reverse($arr));
